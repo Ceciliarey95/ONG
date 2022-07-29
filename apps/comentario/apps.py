cd1eb6a91f76523b6770a7f67ea2d63ad152bfac
Cecilia Rey <ceciliarey95.cr@gmail.com>
2022-07-29 19:44:57 -0300

    hasta 27

---
+++ apps/comentario/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CoemntarioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'coemntario'
--- ong/settings.py
+++ ong/settings.py
@@ -15,18 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+AUTH_USER_MODEL ='usuario.Usuario'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-v^&kl^=vv6n*%nd2@%su*64!scvh3#x1c#q(6#+0ldd+cvwc5_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -37,6 +32,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.noticia',
+    'apps.comentario',
+    'apps.usuario'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +52,7 @@ ROOT_URLCONF = 'ong.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,17 +66,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ong.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -119,11 +106,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 import django_heroku 
 django_heroku.settings(locals())
