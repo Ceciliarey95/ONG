0c926562d97b0282f1b8096ea62cfe8b89bab17d
Cecilia Rey <ceciliarey95.cr@gmail.com>
2022-07-31 23:49:23 -0300

    commit

--- ong/urls.py
+++ ong/urls.py
@@ -16,13 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from .views import Index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Index, name='index'),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('noticia/', include('apps.noticia.urls')),
+    path('usuario/', include('apps.usuario.urls')),
+    path('comentario/', include('apps.comentario.urls')),
+    ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
