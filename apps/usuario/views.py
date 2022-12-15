c9db5b9352f281ad2b8045e219e87800e1eb3397
Cecilia Rey <97458514+Ceciliarey95@users.noreply.github.com>
2022-12-14 22:52:39 -0300

    sin cambios

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, DeleteView
 from .models import Usuario
 from .forms import RegistroUsuarioForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 """ from django.contrib.auth.forms import UserCreationForm
 class CustomUserCreationForm(UserCreationForm):
@@ -17,10 +18,10 @@ class RegistrarUsuario(CreateView):
 	template_name = 'usuario/registrar.html'
 	success_url   = reverse_lazy('index')
 
-def ListarUsuario(request):
+def ListarUsuario(LoginRequiredMixin,request):
     usuario    = Usuario.objects.all()
-    
     context = {
         'usuario':usuario,
     }
     return render(request,'base.html',context)
+
--- ong/prod.py
+++ ong/prod.py
@@ -3,14 +3,22 @@ from .settings import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.101']
+ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'ENGINE'  : 'django.db.backends.postgresql',
+        'NAME'    : 'd94jbupvd88gms',
+        'USER'    : 'gsilccgiowbhtk',
+        'PASSWORD': '24231c3ee4697f9fed435a4d415d3fd0cf275acf8cb71a10dc1082216d7e8499',
+        'HOST'    : 'ec2-44-206-197-71.compute-1.amazonaws.com',
+        'PORT'    : '5432',
     }
 }
+
+
+import django_heroku
+django_heroku.settings(locals())
